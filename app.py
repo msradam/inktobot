19ff4b9f8d254ddb3e303813b5c795987d1c3968
Adam Rahman <mrahmanadam@gmail.com>
2019-10-12 12:53:03 -0400

    Modified port binding in app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template
 from inktobot import get_svg_drawing
+import os
 
 app = Flask(__name__)
 
@@ -12,3 +13,8 @@ def index(drawing=None, currentPrompt=None):
         drawing = None
         currentPrompt = None
     return render_template('index.html', drawing=drawing, currentPrompt=currentPrompt)
+
+if __name__ == '__main__':
+    # Bind to PORT if defined, otherwise default to 5000.
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
